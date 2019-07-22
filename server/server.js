81f45514c943861a51f27d0ae26874a056caa93d
René Winkler <info@rewiso.ch>
2019-07-22 20:26:14 +0200

    handle build and assets

--- server/server.js
+++ server/server.js
@@ -8,6 +8,7 @@ import errorhandler from 'errorhandler';
 import csrf from 'csurf';
 import morgan from 'morgan';
 import favicon from 'serve-favicon';
+import fs from 'fs';
 import path from 'path';
 import router from './routes/router';
 import database from './lib/database';
@@ -19,89 +20,92 @@ const port = process.env.PORT || '3000';
 
 class Server {
 
-    constructor() {
-        // this.initViewEngine(); We don't have a view engine
-        this.initExpressMiddleWare();
-        this.initCustomMiddleware();
-        this.initDbSeeder();
-        this.initRoutes();
-        this.start();
+  constructor() {
+    // this.initViewEngine(); We don't have a view engine
+    this.initExpressMiddleWare();
+    this.initCustomMiddleware();
+    this.initDbSeeder();
+    this.initRoutes();
+    this.start();
+  }
+
+  start() {
+    app.listen(port, (err) => {
+      console.log('[%s] Listening on http://localhost:%d', process.env.NODE_ENV, port);
+    });
+  }
+
+  // initViewEngine() {
+  //     const hbs = exphbs.create({
+  //         extname: '.hbs',
+  //         defaultLayout: 'master'
+  //     });
+  //     app.engine('hbs', hbs.engine);
+  //     app.set('view engine', 'hbs');
+  //     hbsLayouts.register(hbs.handlebars, {});
+  // }
+
+  initExpressMiddleWare() {
+    const faviconPath = __dirname + '/public/favicon.ico';
+    if (fs.existsSync(faviconPath)) {
+      app.use(favicon(faviconPath));
+    }
+    app.use(express.static(path.join(__dirname, '/public')));
+    app.use(morgan('dev'));
+    app.use(bodyParser.urlencoded({extended: true}));
+    app.use(bodyParser.json());
+    app.use(errorhandler());
+    app.use(cookieParser());
+    app.use(csrf({cookie: true}));
+
+    app.use((req, res, next) => {
+      let csrfToken = req.csrfToken();
+      res.locals._csrf = csrfToken;
+      res.cookie('XSRF-TOKEN', csrfToken);
+      next();
+    });
+
+    process.on('uncaughtException', (err) => {
+      if (err) console.log(err, err.stack);
+    });
+  }
+
+  initCustomMiddleware() {
+    if (process.platform === "win32") {
+      require("readline").createInterface({
+        input: process.stdin,
+        output: process.stdout
+      }).on("SIGINT", () => {
+        console.log('SIGINT: Closing MongoDB connection');
+        database.close();
+      });
     }
 
-    start() {
-        app.listen(port, (err) => {
-            console.log('[%s] Listening on http://localhost:%d', process.env.NODE_ENV, port);
-        });
-    }
+    process.on('SIGINT', () => {
+      console.log('SIGINT: Closing MongoDB connection');
+      database.close();
+    });
+  }
 
-    // initViewEngine() {
-    //     const hbs = exphbs.create({
-    //         extname: '.hbs',
-    //         defaultLayout: 'master'
-    //     });
-    //     app.engine('hbs', hbs.engine);
-    //     app.set('view engine', 'hbs');
-    //     hbsLayouts.register(hbs.handlebars, {});
-    // }
+  initDbSeeder() {
+    database.open(() => {
+      //Set NODE_ENV to 'development' and uncomment the following if to only run
+      //the seeder when in dev mode
+      //if (process.env.NODE_ENV === 'development') {
+      //  seeder.init();
+      //}
+      seeder.init();
+    });
+  }
 
-    initExpressMiddleWare() {
-        app.use(favicon(__dirname + '/public/images/favicon.ico'));
-        app.use(express.static(path.join(__dirname, '/public')));
-        app.use(morgan('dev'));
-        app.use(bodyParser.urlencoded({extended: true}));
-        app.use(bodyParser.json());
-        app.use(errorhandler());
-        app.use(cookieParser());
-        app.use(csrf({cookie: true}));
+  initRoutes() {
+    router.load(app, './controllers');
 
-        app.use((req, res, next) => {
-            let csrfToken = req.csrfToken();
-            res.locals._csrf = csrfToken;
-            res.cookie('XSRF-TOKEN', csrfToken);
-            next();
-        });
-
-        process.on('uncaughtException', (err) => {
-            if (err) console.log(err, err.stack);
-        });
-    }
-
-    initCustomMiddleware() {
-        if (process.platform === "win32") {
-            require("readline").createInterface({
-                input: process.stdin,
-                output: process.stdout
-            }).on("SIGINT", () => {
-                console.log('SIGINT: Closing MongoDB connection');
-                database.close();
-            });
-        }
-
-        process.on('SIGINT', () => {
-            console.log('SIGINT: Closing MongoDB connection');
-            database.close();
-        });
-    }
-
-    initDbSeeder() {
-        database.open(() => {
-            //Set NODE_ENV to 'development' and uncomment the following if to only run
-            //the seeder when in dev mode
-            //if (process.env.NODE_ENV === 'development') {
-            //  seeder.init();
-            //}
-            seeder.init();
-        });
-    }
-
-    initRoutes() {
-        router.load(app, './controllers');
-
-        // redirect all others to the index (HTML5 history)
-        app.all('/*', (req, res) => {
-            res.sendFile(__dirname + '/public/index.html');
-        });
-    }
+    // redirect all others to the index (HTML5 history)
+    app.all('/*', (req, res) => {
+      res.sendFile(__dirname + '/public/index.html');
+    });
+  }
 
 }
 
