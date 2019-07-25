beb922a52226d6772635311826b3e191d56af9a5
René Winkler <info@rewiso.ch>
2019-07-25 12:23:20 +0200

    initial commit: frontend

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,19 @@
+import { NgModule }      from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
 
+import { AppComponent }  from './app.component';
 import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
+import { CoreModule }   from './core/core.module';
+import { SharedModule }   from './shared/shared.module';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    CoreModule,   //Singleton objects
+    SharedModule  //Shared (multi-instance) objects
   ],
-  providers: [],
-  bootstrap: [AppComponent]
+  declarations: [ AppComponent, AppRoutingModule.components ],
+  bootstrap:    [ AppComponent ]
 })
 export class AppModule { }
